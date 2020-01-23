e55edb992e5dd3380eda6e4ee16277bf097d44e2
tempest <soccer.i_y@icloud.com>
2020-01-24 01:30:27 +0900

    commit

--- jquery.js
+++ jquery.js
@@ -1,2 +1,7 @@
 $(".title").hide();
 $('.title').slideDown(1200);
+// $(window).load(function () {
+//     // 消えるタイミングはお好みで
+//     $('.loading').fadeOut(300);
+// });
+$('.loading').delay(1300).fadeOut(300);
